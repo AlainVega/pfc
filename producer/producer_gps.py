a0e9b1597fa532a35267a49a69f428579d226046
Alain Vega <alainjosevz@gmail.com>
2025-10-09 23:34:18 -0300

    desfiltrar por lineae id en producer_gps

--- producer/producer_gps.py
+++ producer/producer_gps.py
@@ -22,12 +22,15 @@ file_path = os.path.abspath(os.path.join(os.path.abspath(__file__), '../Itinerar
 
 gdf = gpd.read_file(file_path, driver='geojson')
 
+# gdf = gdf[(gdf['linea']=='11')&(gdf['id']=='5')]
+
 # %%
 
 # Número de puntos GPS a simular:
 n_points = 100
+loop_inf = True
 
-def enviar_ubicacion_colectivo(linea: str, id_: str, empresa: str, geometry, n_points=5):
+def enviar_ubicacion_colectivo(linea: str, id_: str, empresa: str, geometry, n_points=5, loop_inf=True):
     """ Función para simular datos GPS de buses y enviarlos a Kafka """
 
     # Reproyectamos a metros (UTM 21S para Paraguay):
@@ -37,21 +40,39 @@ def enviar_ubicacion_colectivo(linea: str, id_: str, empresa: str, geometry, n_p
     # Convertir de vuelta a lat/lon (EPSG:4326):
     points = gpd.GeoSeries(points_proj, crs="EPSG:32721").to_crs(epsg=4326)
 
-    # Enviar cada punto como un mensaje Kafka
-    for idx, pt in enumerate(points):
-        mensaje = {
-            "linea": linea,
-            "id": str(id_),
-            "empresa": empresa,
-            "timestamp": time.strftime("%Y-%m-%dT%H:%M:%S"),
-            "lat": pt.y,
-            "lon": pt.x,
-            "sequence": idx
-        }
+    if loop_inf:
+    
+        while True:
+            # Enviar cada punto como un mensaje Kafka
+            for idx, pt in enumerate(points):
+                mensaje = {
+                    "linea": linea,
+                    "id": str(id_),
+                    "empresa": empresa,
+                    "timestamp": time.strftime("%Y-%m-%dT%H:%M:%S"),
+                    "lat": pt.y,
+                    "lon": pt.x,
+                    "sequence": idx
+                }
 
-        producer.produce(topic, value=json.dumps(mensaje)) # enviar mensaje
-        producer.poll(0) # procesa callbacks
-        time.sleep(5)  # velocidad de simulación
+                producer.produce(topic, value=json.dumps(mensaje)) # enviar mensaje
+                producer.poll(0) # procesa callbacks
+                time.sleep(5)  # velocidad de simulación
+    else:
+        for idx, pt in enumerate(points):
+            mensaje = {
+                "linea": linea,
+                "id": str(id_),
+                "empresa": empresa,
+                "timestamp": time.strftime("%Y-%m-%dT%H:%M:%S"),
+                "lat": pt.y,
+                "lon": pt.x,
+                "sequence": idx
+            }
+
+            producer.produce(topic, value=json.dumps(mensaje)) # enviar mensaje
+            producer.poll(0) # procesa callbacks
+            time.sleep(5)  # velocidad de simulación
 
 # %%
 # Crear y lanzar un hilo por cada colectivo
@@ -61,7 +82,7 @@ for (linea, id_), grupo in gdf.groupby(["linea", "id"]):
     empresa = grupo.iloc[0].get("empresa", "Desconocida")
     geometry = grupo.iloc[0]["geometry"]
     
-    t = threading.Thread(target=enviar_ubicacion_colectivo, args=(linea, id_, empresa, geometry, n_points))
+    t = threading.Thread(target=enviar_ubicacion_colectivo, args=(linea, id_, empresa, geometry, n_points, loop_inf))
     threads.append(t)
     t.start()
 
